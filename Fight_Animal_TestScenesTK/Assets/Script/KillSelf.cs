ccc8188d9833f7b8a481ac05eb862c81b3a2dea3
Homaru <D.K.R@hotmail.com>
2018-04-19 15:17:12 +0900

    命中ボタンと回避ボタンの追加

---
+++ Fight_Animal_TestScenesTK/Assets/Script/KillSelf.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillSelf : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("KillMe",1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void KillMe()
+    {
+        Destroy(this.gameObject);
+    }
+}
